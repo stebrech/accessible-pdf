d85e52698e5728c90fcff04ce79bab9bf3b2b90d
pixelstrolch <email@pixelstrol.ch>
2021-01-29 13:48:12 +0100

    Localize and improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,40 @@
 import React from 'react'
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import { useIntl } from "react-intl"
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import style from './404.module.css'
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = ({ data }) => {
 
+  const intl = useIntl()
+
+  return (
+    <Layout>
+      <SEO title={intl.formatMessage({ id: "error.title" })} />
+
+      <div className={style.container}>
+        <h1 className={style.title}>{intl.formatMessage({ id: "error.title" })}</h1>
+        <p className={style.description}>{intl.formatMessage({ id: "error.description" })}</p>
+        <Img className={style.dog} fluid={data.dogImage.childImageSharp.fluid} alt={intl.formatMessage({ id: "error.dog" })} />
+      </div>
+    </Layout>
+  )
+}
 export default NotFoundPage
+
+export const query = graphql`
+  {
+    dogImage: file(
+      relativePath: { eq: "dog.jpg" }
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 1080) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
