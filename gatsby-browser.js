a53eaddb9ece1e6e869eb2ba279d379313f80408
pixelstrolch <email@pixelstrol.ch>
2021-07-26 22:01:52 +0200

    ServiceWorker update message

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,13 @@ import "./src/styles/reset.css"
 import "./src/styles/global.module.css"
 import "./src/styles/accessibility.css"
 import "./src/styles/general.css"
+import { useIntl } from "react-intl"
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    useIntl.formatMessage({ id: "sw.outdated" })
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
